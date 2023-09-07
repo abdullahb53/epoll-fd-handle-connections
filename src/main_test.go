5f36c901456d07103a4821dc42ec3f612eb7fe51
abdullahb53 <abdullahb5355@gmail.com>
2023-09-07 21:20:55 +0300

    test-added

--- src/main_test.go
+++ src/main_test.go
@@ -6,18 +6,33 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const N = 100
+
 func TestPool(t *testing.T) {
-	poolsize := 100
+	poolsize := 20
 	pool := NewPool(poolsize)
 	for i := 0; i < poolsize; i++ {
 		pool.Schedule(func() {})
 	}
 
 	values := make([]int, 0)
-	for i := 0; i < poolsize; i++ {
+	for i := 0; i < N; i++ {
+		val := i
 		pool.Schedule(func() {
-			values = append(values, i)
+			values = append(values, val+1)
 		})
 	}
-	assert.Equal(t, poolsize-1, len(values))
+
+	// Imitate some other stuffs.
+	pool.Schedule(func() {})
+	pool.Schedule(func() {})
+	pool.Schedule(func() {})
+
+	sum := 0
+	for i := 0; i < len(values); i++ {
+		sum += values[i]
+	}
+	t.Log("values:", values, "len:", len(values))
+	assert.Equal(t, sum, (N)*(N+1)/2)
+
 }
